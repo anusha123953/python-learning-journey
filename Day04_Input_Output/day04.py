095122a4d385586547a5f7d6eb0215e9f62d771a
Anusha <anushashetty932@gmail.com>
2026-04-09 20:13:38 +0530

    Added Day 04 python code
    Added input prompts for name and age, and formatted output using f-string.

--- Day04_Input_Output/day04.py
+++ Day04_Input_Output/day04.py
@@ -1,1 +1,9 @@
+# Taking input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
 
+# Output
+print("Hello", name)
+
+# f-string
+print(f"My name is {name} and I am {age} years old.
